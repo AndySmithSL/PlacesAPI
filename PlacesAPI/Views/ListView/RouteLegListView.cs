54bdaf77fe6c2da012f8ac41c80a47568e1df2d3
Andy Smith <asmith@ScottLogic.com>
2020-02-07 17:22:11 +0000

    Route Leg changes

--- PlacesAPI/Views/ListView/RouteLegListView.cs
+++ PlacesAPI/Views/ListView/RouteLegListView.cs
@@ -5,7 +5,13 @@ namespace PlacesAPI.Views.ListView
     public class RouteLegListView : RouteLegView
     {
         public string Route => ViewObject.Route.Name;
-        public string Origin => ViewObject.Origin.Name;
+        
         public string Destination => ViewObject.Destination.Name;
+
+        //TODO:AS
+        public string Origin => ViewObject.Origin.Name;
+        public double? OriginLat => ViewObject.Origin.Latitude;
+        public double? OriginLng => ViewObject.Origin.Longitude;
+
     }
 }
